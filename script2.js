099a5f2c7944b6b6288c59a6a62f8f724cae907b
Dimynkyl <pavelmatvey234@gmail.com>
2024-06-12 23:43:05 +0500

    Delete script2.js

--- script2.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.pop_up_body input').forEach(input => {
-  input.addEventListener('focus', () => {
-    input.classList.add('select-clicked');
-  });
-
-  input.addEventListener('blur', () => {
-    input.classList.remove('select-clicked');
-  });
-});
